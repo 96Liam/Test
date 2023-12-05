fb18ca51be361fe1c1061d4f88a854d7eb3d9008
96Liam <152358830+96Liam@users.noreply.github.com>
2023-12-05 17:08:52 +0800

    Add files via upload

--- calculation.js
+++ calculation.js
@@ -5,12 +5,14 @@ function calculate() {
   // 检查输入的值是否有效
   if (isNaN(kilometers) || isNaN(hours) || kilometers <= 0 || hours <= 0) {
     document.getElementById('result').innerHTML = '低能兒';
+    document.getElementById('deposit').innerHTML = '你確定要租車?????';
     document.getElementById('depositResult').innerHTML = '輸入大於0的數字啦';
   } else {
-    var result = kilometers * 4.5 + hours * 140;
-    var deposit = hours * 140; // 計算訂金
+    var result = (kilometers * 4.5 + hours * 140).toFixed(0);
+    var firstpay = (hours * 140).toFixed(0); // 計算訂金
 
     document.getElementById('result').innerHTML = '預期總金額為： NT$ ' + result;
-    document.getElementById('depositResult').innerHTML = '訂金為： NT$ ' + deposit + ' <br> (先收取時間計費，里程計費等還車時再收費)';
+    document.getElementById('deposit').innerHTML = '訂金： NT$ ' + (firstpay*0.3).toFixed(0) ;
+    document.getElementById('depositResult').innerHTML = '取車時需先收取： NT$ ' + firstpay + ' <br> (先收取時間計費，里程計費等還車時再收費)';
   }
 }
