3bfb72cff11277847851da183827cf5809ff2ee3
96Liam <152358830+96Liam@users.noreply.github.com>
2023-12-05 11:31:38 +0800

    Add files via upload

---
+++ calculation.js
@@ -0,0 +1,10 @@
+function calculate() {
+  var kilometers = parseFloat(document.getElementById('kilometers').value);
+  var hours = parseFloat(document.getElementById('hours').value);
+
+  var result = kilometers * 4.5 + hours * 140;
+  var deposit = hours * 140; // 計算訂金
+
+  document.getElementById('result').innerHTML = '預期總金額為：' + result;
+  document.getElementById('depositResult').innerHTML = '訂金為：' + deposit +' (先收取時間計費，里程計費等還車時再收費)'; // 顯示訂金結果
+}
