411a544b2b6c148ce591cb31e09ffd650d0a37bb
96Liam <152358830+96Liam@users.noreply.github.com>
2023-12-05 12:41:23 +0800

    Add files via upload

--- calculation.js
+++ calculation.js
@@ -6,7 +6,6 @@ function calculate() {
   var deposit = hours * 140; // 計算訂金
 
   document.getElementById('result').innerHTML = '預期總金額為：' + result;
-
-  document.getElementById('depositResult').innerHTML = '訂金為：' + deposit + '<br>(先收取時間計費<br>里程計費等還車時再收費)';
+  document.getElementById('depositResult').innerHTML = '訂金為：' + deposit + ' <br> (先收取時間計費，里程計費等還車時再收費)';
  // 顯示訂金結果
 }
