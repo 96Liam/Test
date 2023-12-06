03755f859915a4a8d30776b7f46be3f794fe58ce
96Liam <152358830+96Liam@users.noreply.github.com>
2023-12-06 10:27:14 +0800

    Add files via upload

--- calculation.js
+++ calculation.js
@@ -8,14 +8,25 @@ function calculate() {
     document.getElementById('deposit').innerHTML = '你確定要租車?????';
     document.getElementById('depositResult').innerHTML = '輸入大於0的數字啦';
   } else {
-    var result = (kilometers * 4.5 + hours * 140).toFixed(0);
-    var firstpay = (hours * 140).toFixed(0); 
+    var result;
+    var firstpay;
+    if (hours >= 108) {
+      result = (kilometers * 4.5 + hours * 140 * 0.8).toFixed(0);
+      firstpay = (hours * 140 * 0.8).toFixed(0);
+    } else if (hours >= 60) {
+      result = (kilometers * 4.5 + hours * 140 * 0.9).toFixed(0);
+      firstpay = (hours * 140 * 0.9).toFixed(0);
+    } else {
+      result = (kilometers * 4.5 + hours * 140).toFixed(0);
+      firstpay = (hours * 140).toFixed(0);
+    }
+    
 
     document.getElementById('result').innerHTML = '預期總金額為： NT$ ' + result;
     var depositAmount = (firstpay * 0.3).toFixed(0); 
     var finalAmount = (firstpay - depositAmount).toFixed(0); 
 
     document.getElementById('deposit').innerHTML = '訂金： NT$ ' + depositAmount;
-    document.getElementById('depositResult').innerHTML = '取車時需先收取(已扣訂金)： NT$ ' + finalAmount + ' <br> (先收取時間計費，里程計費等還車時再收費)';
+    document.getElementById('depositResult').innerHTML = '取車時需先收取(已扣訂金)： NT$ ' + finalAmount + ' <br> (先收取時間計費，里程計費待還車時另收費)';
   }
 }
